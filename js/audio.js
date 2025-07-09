0f2e961a96d55e19278b8edc41dc882a63b218f5
digitald <ugo.romano@gmail.com>
2025-07-09 17:57:08 +0200

    MVP 2 nuovo audio e UI

--- js/audio.js
+++ js/audio.js
@@ -5,12 +5,14 @@ export function playInterval(freq1, freq2) {
   gain.connect(ctx.destination);
 
   const osc1 = ctx.createOscillator();
+  osc1.type = 'triangle'; // o 'sawtooth'
   osc1.frequency.value = freq1;
   osc1.connect(gain);
   osc1.start(ctx.currentTime);
   osc1.stop(ctx.currentTime + 1);
 
   const osc2 = ctx.createOscillator();
+  osc2.type = 'triangle'; // o 'sawtooth'
   osc2.frequency.value = freq2;
   osc2.connect(gain);
   osc2.start(ctx.currentTime + 1);
